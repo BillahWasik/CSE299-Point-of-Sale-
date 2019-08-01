8120422b1c90a24fc6f0e690412945e05718e94c
Wasik <wasik.farhad@northsouth.edu>
2019-08-02 01:46:15 +0600

    Feature #52 : Product Price Check updated

--- Project Code/PHP files/checkprice.php
+++ Project Code/PHP files/checkprice.php
@@ -148,15 +148,4 @@ if (isset($_POST['entry'])) {
 ?>
 
 </body>
-<script>
-    'undefined' === typeof _trfq || (window._trfq = []);
-    'undefined' === typeof _trfd && (window._trfd = []), _trfd.push({
-        'tccl.baseHost': 'secureserver.net'
-    }), _trfd.push({
-        'ap': 'cpsh'
-    }, {
-        'server': 'a2plcpnl0354'
-    })
-</script>
-<script src='https://img1.wsimg.com/tcc/tcc_l.combined.1.0.6.min.js'></script>
 </html>
