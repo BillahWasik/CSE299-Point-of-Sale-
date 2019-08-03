2cbdf5633043ec8cb1e47ef02c228c1065a35930
Wasik <wasik.farhad@northsouth.edu>
2019-08-04 01:42:15 +0600

    Feature #54 : Account section page updated

--- Project Code/PHP files/totalcash.php
+++ Project Code/PHP files/totalcash.php
@@ -129,19 +129,76 @@ if (!isset($_SESSION['pos_admin']) || !isset($_COOKIE['userlog'])) {
         <div class="total-cash">
             <div class="row">
                 <div class="col-sm-6">
-                    <h4 class="total">Total Cash:</h4>
+                    <h4 class="total">Total Cash:
+                        <?php echo sprintf('%.2f', cash($db) ); ?> BDT</h4>
                 </div>
                 <div class="col-md-6">
                     <form method="post" class="form-inline" style="float: right;">
                         <div class="form-group">
 
-                            <input type="month" id="start" class="form-control" name="short_month" min="2017-01" value="2018-11" />
+                            <input type="month" id="start" class="form-control" name="short_month" min="2019-01" value="<?php echo date('Y-m'); ?>" />
 
                         </div>
                         <button type="submit" class="btn btn-default add" style="font-size: 12px; font-weight: normal; line-height: 0.7; padding: 11px 12px;" name="select_">SELECT</button>
                     </form>
                 </div>
             </div>
+
+            <?php
+            if(isset($_POST['select_']) && !empty($_POST['short_month'])){
+                $month=$_POST['short_month'];
+                $sql3 = "SELECT * FROM pos_ex WHERE date LIKE '$month%' ORDER BY id DESC";
+            }
+            else
+                {
+
+                    $sql3 = "SELECT * FROM pos_ex ORDER BY id DESC";
+
+            }
+            $result3 = mysqli_query($db, $sql3);
+            $shop_cash = 0.00;
+            if(mysqli_num_rows($result3) > 0){
+                while($record3 = mysqli_fetch_assoc($result3)){
+                    if($record3['status']==FALSE){
+                        //echo "(+) ";
+                        $shop_cash = $shop_cash + floatval($record3['amount']);
+                    }else{
+                        //echo "(-) ";
+                        $shop_cash = $shop_cash - floatval($record3['amount']);
+                    }
+
+                }
+            }
+
+            $extra_cash = $shop_cash;
+            $net_sale =0.0;
+            $card_payment =0.0;
+            $cash_payment =0.0;
+            $net_discount =0.0;
+            if(isset($_POST['select_']) && !empty($_POST['short_month'])){
+                $month=$_POST['short_month'];
+                $sql = "SELECT * FROM pos_sale WHERE date LIKE '$month%' ORDER BY sale_id DESC";
+            }else{
+                $sql = "SELECT * FROM pos_sale ORDER BY sale_id DESC";
+
+            }
+            $result = mysqli_query($db, $sql);
+
+            if(mysqli_num_rows($result) > 0){
+                while($record = mysqli_fetch_assoc($result)){
+
+
+                    $shop_cash = $shop_cash + floatval($record['net_price']);
+                    $net_sale = $net_sale + floatval($record['net_price']);
+
+                    $cash_payment= $cash_payment + floatval($record['net_price']);
+                    $discount=$record['final_discount']+$record['discount_round'];
+                    $net_discount = $net_discount + floatval($discount);
+
+
+                }
+            }
+            ?>
 
 
             <div class="row">
